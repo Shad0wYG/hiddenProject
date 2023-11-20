321a9497d6864c4aa3cc94e1756e4e41cbe065c8
Ispas Anisia <ispasanisia22@stud.ase.ro>
2023-11-20 22:43:12 +0200

    Defined Getters, Setters and Parameter constructor for the first class.
    Made a rudimentary map of the seating

--- Project1/ClassDefinitions.h
+++ Project1/ClassDefinitions.h
@@ -3,6 +3,111 @@
 
 class Ticket {
 
+	/// Attributes & Defaults
+
+private:
+	string id = "";
+	int row = -1;
+	int seat = -1;
+	bool isVIPSeat = false;
+	bool isSpecialNeedsSeat = false;
+
+public:
+
+	/// Magic numbers
+	static const int ID_LENGTH = 6;
+	static const int MIN_ROW_NO = 1;
+	static const int MAX_ROW_NO = 7;
+	static const int MIN_SEAT_NO = 1;
+	static const int MAX_SEAT_NO = 15;
+
+
+	/// Getters
+	
+		string getID() {
+			return this->id;
+
+		}
+
+		int getRow() {
+			return this->row;
+		}
+
+		int getSeat() {
+			return this->seat;
+		}
+
+		bool getVIPSeat() {
+			return this->isVIPSeat;
+		}
+
+		bool getSpecialNeedsSeat() {
+			return this->isSpecialNeedsSeat;
+		}
+
+	
+
+	/// Setters
+
+	void setID(string ID) {
+
+		if (ID.size() != ID_LENGTH) {
+			throw exception("ID size is invalid");
+		}
+
+		this->id = ID;
+	}
+
+	void setRow(int newRow) {
+		if (newRow < MIN_ROW_NO || newRow > MAX_ROW_NO) {
+			throw exception("Row number is invalid");
+		}
+
+		this->row = newRow;
+	}
+
+	void setSeat(int newSeat) {
+		if (newSeat < MIN_SEAT_NO || newSeat > MAX_SEAT_NO) {
+			throw exception("Seat number is invalid");
+		}
+
+		this->seat = newSeat;
+	}
+
+
+	/// WILL FIX LATER I PROMISE 
+
+
+	void getsVIPSeat() {
+		this->isVIPSeat = true;
+	}
+	void getsNoVIPSeat() {
+		this->isVIPSeat = false;
+	}
+
+	void getsSpecialNeedsSeat() {
+		this->isSpecialNeedsSeat = true;
+	}
+
+	void getsNoSpecialNeedsSeat() {
+		this->isSpecialNeedsSeat = false;
+	}
+
+
+	///WILL FIX THIS TOO 
+
+
+	/// Constructor
+	Ticket(string code, int row, int seat, bool isVIPSeat, bool isSpecialNeedsSeat) {
+		this->id = code;
+		this->row = row;
+		this->seat = seat;
+		this->isVIPSeat = isVIPSeat;
+		this->isSpecialNeedsSeat = isSpecialNeedsSeat;
+		
+
+	}
+
 
 };
 
--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
-#include "ClassDefinitions.h";
+#include <string>
+//#include "ClassDefinitions.h";
 
+using namespace std;
+
+int SeatMatrix[8][16];
+
+void createSeatMatrix(int seatmatrix[8][16]) {
+	for (int i = 0; i < 2; i++) 
+		for (int j = 0; j < 15; j++) seatmatrix[i][j] = 1;
+
+	for (int i = 2; i < 5; i++) {
+
+		for (int j = 0; j < 2; j++) seatmatrix[i][j] = 2;
+		for (int j = 2; j < 13; j++) seatmatrix[i][j] = 0;
+		for (int j = 13; j < 15; j++) seatmatrix[i][j] = 2;
+
+
+	}
+	for (int i = 5; i < 7; i++) {
+			for (int j = 0; j < 15; j++) seatmatrix[i][j] = 1;
+			
+		}
+}
+
+
+void printSeatMatrix(int seatmatrix[8][16]) {
+	for (int i = 0; i < 7; i++) {
+		for (int j = 0; j < 15; j++) cout << seatmatrix[i][j] << " ";
+		cout << "\n";
+	}
+
+}
 
 int main() {
-
+	createSeatMatrix(SeatMatrix);
+	printSeatMatrix(SeatMatrix);
 
 }
